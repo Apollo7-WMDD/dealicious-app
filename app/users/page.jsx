e9afb657ad9f2011ad3c99423d328cd847dcd117
Tony-Thawatchai <au.thawatchai@gmail.com>
2023-06-14 09:24:06 -0700

    feat : AP-97 : add zustand state management

--- app/users/page.jsx
+++ app/users/page.jsx
@@ -1,15 +1,22 @@
 "use client";
 import { useState, useEffect } from "react";
+import { useStore } from "../store";
 
 const User = () => {
-  const [users, setUsers] = useState([]);
+  // const [users, setUsers] = useState([]);
+
+  // IMPORT HOOK FROM STATE MANAGEMENT STORE
+  const filter = useStore((state) => state.filter);
+  const setFilter = useStore((state) => state.setFilter);
+  const user = useStore((state) => state.user);
+  const setUser = useStore((state) => state.setFilter);
 
   useEffect(() => {
     const fetchUsers = async () => {
       const res = await fetch("/api/users");
       const data = await res.json();
-      setUsers(data);
-      console.log(data);
+      setUser(data);
+      
     };
     fetchUsers();
   }, []);
@@ -17,12 +24,20 @@ const User = () => {
   return (
     <>
       <h1 className="text-4xl">TESTING DEV</h1>
+{console.log(user)}
+      <input
+        type="text"
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
+      />
 
-      {users.map((user) => (
-        <div key={user._id}>
-          <h2 className="text-2xl text-red-700 m-5">{user.name}</h2>
-        </div>
-      ))}
+      <div>
+        {user.filter((email) => email.toLowerCase().includes(filter.toLowerCase())).map((user) => (
+          <div key={user._id}>
+            <h2 className="text-2xl text-red-700 m-5">{user.email}</h2>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
