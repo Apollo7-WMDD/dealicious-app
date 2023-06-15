cb884a7ae4159e542893238d4d70640827b21a45
Mario Cesena <112787277+mariocc22@users.noreply.github.com>
2023-06-15 16:54:46 -0700

    feat: AP-101: Adding the Campaigns Page

--- app/campaigns/page.jsx
+++ app/campaigns/page.jsx
@@ -1,7 +1,117 @@
-import React from "react";
+"use client";
+import { set } from "mongoose";
+import { useEffect, useState } from "react";
 
 const Campaigns = () => {
-  return <div>Campaigns</div>;
+  const [campaigns, setCampaigns] = useState([]);
+
+  // fetch all campaigns from the database
+  useEffect(() => {
+    const fetchCampaigns = async () => {
+      const res = await fetch("/api/campaigns", {
+        cache: "no-store",
+      });
+      if (!res.ok) throw new Error("Something went wrong...");
+
+      const data = await res.json();
+      setCampaigns(data);
+      console.log(data);
+    };
+    fetchCampaigns();
+  }, []);
+
+  //submit function
+  const handleSubmit = async function (e) {
+    e.preventDefault();
+    const formData = {
+      restaurantId: e.target.restaurantId.value,
+      name: e.target.name.value,
+      availableCodes: e.target.availableCodes.value,
+      startDate: e.target.startDate.value,
+      endDate: e.target.endDate.value,
+    };
+    try {
+      const res = await fetch("/api/campaigns", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      if (!res.ok) {
+        const data = await res.text();
+        throw new Error(data);
+      } else {
+        const data = await res.json();
+        console.log("Success! ", data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    e.target.reset();
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center text-center">
+      <h1 className="text-4xl mb-5  block">Campaigns</h1>
+      {campaigns.map((campaign) => (
+        <div key={campaign._id}>
+          <h2>{campaign.name}</h2>
+          <p>{campaign.availableCodes}</p>
+        </div>
+      ))}
+      <h2 className="text-4xl mb-5 text-center">Add a Campaign</h2>
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center flex-col gap-2"
+      >
+        <label htmlFor="name">RestaurantID</label>
+        <input
+          className="border-solid border-green-700 border-2 rounded-md"
+          type="text"
+          name="restaurantId"
+          id="restaurantId"
+        />
+        <label htmlFor="name">Name</label>
+        <input
+          className="border-solid border-green-700 border-2 rounded-md"
+          type="text"
+          name="name"
+          id="name"
+        />
+
+        <label htmlFor="availableCodes">Available Codes</label>
+        <input
+          className="border-solid border-green-700 border-2 rounded-md"
+          type="number"
+          name="availableCodes"
+          id="availableCodes"
+        />
+
+        <label htmlFor="startDate">Start Date</label>
+        <input
+          className="border-solid border-green-700 border-2 rounded-md"
+          type="date"
+          name="startDate"
+          id="startDate"
+        />
+
+        <label htmlFor="endDate">End Date</label>
+        <input
+          className="border-solid border-green-700 border-2 rounded-md"
+          type="date"
+          name="endDate"
+          id="endDate"
+        />
+        <button
+          type="submit"
+          className=" mx-2 px-4 py-2 border-solid border-red-700 border-2 rounded-md bg-red-700 text-white"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Campaigns;
--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,27 @@
-import Image from 'next/image'
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      {/* <h1>Home</h1>
-      <p>Random Text...</p> */}
-      <Link href="/users" className='text-red-500'>Users</Link>
+      <Link
+        href="/users"
+        className=" mx-2 px-4 py-2 border-solid border-red-700 border-2 rounded-md bg-red-700 text-white"
+      >
+        <button className="">Users</button>
+      </Link>
+      <Link
+        href="/campaigns"
+        className=" mx-2 px-4 py-2 border-solid border-red-700 border-2 rounded-md bg-red-700 text-white"
+      >
+        <button className="">Campaigns</button>
+      </Link>
+      <Link
+        href="/points"
+        className=" mx-2 px-4 py-2 border-solid border-red-700 border-2 rounded-md bg-red-700 text-white"
+      >
+        <button className="">Points</button>
+      </Link>
     </main>
-  )
+  );
 }
