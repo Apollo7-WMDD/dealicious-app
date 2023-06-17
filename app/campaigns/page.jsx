9bc5f8877ed3b8f581bfb0f80060210fea03a32c
Mario Cesena <112787277+mariocc22@users.noreply.github.com>
2023-06-16 19:12:01 -0700

    fix: AP-101: style changes in Campaigns page

--- app/campaigns/page.jsx
+++ app/campaigns/page.jsx
@@ -50,64 +50,68 @@ const Campaigns = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center text-center">
-      <h1 className="text-4xl mb-5  block">Campaigns</h1>
-      {campaigns.map((campaign) => (
-        <div key={campaign._id}>
-          <h2>{campaign.name}</h2>
-          <p>{campaign.availableCodes}</p>
-        </div>
-      ))}
-      <h2 className="text-4xl mb-5 text-center">Add a Campaign</h2>
-      <form
-        onSubmit={handleSubmit}
-        className="flex items-center flex-col gap-2"
-      >
-        <label htmlFor="name">RestaurantID</label>
-        <input
-          className="border-solid border-green-700 border-2 rounded-md"
-          type="text"
-          name="restaurantId"
-          id="restaurantId"
-        />
-        <label htmlFor="name">Name</label>
-        <input
-          className="border-solid border-green-700 border-2 rounded-md"
-          type="text"
-          name="name"
-          id="name"
-        />
-
-        <label htmlFor="availableCodes">Available Codes</label>
-        <input
-          className="border-solid border-green-700 border-2 rounded-md"
-          type="number"
-          name="availableCodes"
-          id="availableCodes"
-        />
-
-        <label htmlFor="startDate">Start Date</label>
-        <input
-          className="border-solid border-green-700 border-2 rounded-md"
-          type="date"
-          name="startDate"
-          id="startDate"
-        />
-
-        <label htmlFor="endDate">End Date</label>
-        <input
-          className="border-solid border-green-700 border-2 rounded-md"
-          type="date"
-          name="endDate"
-          id="endDate"
-        />
-        <button
-          type="submit"
-          className=" mx-2 px-4 py-2 border-solid border-red-700 border-2 rounded-md bg-red-700 text-white"
+    <div className="grid grid-cols-2 gap-16">
+      <div>
+        <h1 className="text-4xl mb-5  block">Campaigns</h1>
+        {campaigns.map((campaign) => (
+          <div key={campaign._id}>
+            <h2>{campaign.name}</h2>
+            <p>{campaign.availableCodes}</p>
+          </div>
+        ))}
+      </div>
+      <div>
+        <h2 className="text-4xl mb-5 text-center">Add a Campaign</h2>
+        <form
+          onSubmit={handleSubmit}
+          className="flex items-center flex-col gap-2"
         >
-          Submit
-        </button>
-      </form>
+          <label htmlFor="name">RestaurantID</label>
+          <input
+            className="border-solid border-green-700 border-2 rounded-md"
+            type="text"
+            name="restaurantId"
+            id="restaurantId"
+          />
+          <label htmlFor="name">Name</label>
+          <input
+            className="border-solid border-green-700 border-2 rounded-md"
+            type="text"
+            name="name"
+            id="name"
+          />
+
+          <label htmlFor="availableCodes">Available Codes</label>
+          <input
+            className="border-solid border-green-700 border-2 rounded-md"
+            type="number"
+            name="availableCodes"
+            id="availableCodes"
+          />
+
+          <label htmlFor="startDate">Start Date</label>
+          <input
+            className="border-solid border-green-700 border-2 rounded-md"
+            type="date"
+            name="startDate"
+            id="startDate"
+          />
+
+          <label htmlFor="endDate">End Date</label>
+          <input
+            className="border-solid border-green-700 border-2 rounded-md"
+            type="date"
+            name="endDate"
+            id="endDate"
+          />
+          <button
+            type="submit"
+            className=" mx-2 px-4 py-2 border-solid border-red-700 border-2 rounded-md bg-red-700 text-white"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
