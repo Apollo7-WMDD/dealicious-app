b8fdc3bad8eb421ef71c97a68c4be3312336652b
Mario Cesena <112787277+mariocc22@users.noreply.github.com>
2023-07-01 12:51:31 -0700

    chore: AP-##: Comment out the Theme Provider for testing

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,7 +10,6 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  
   return (
     <html lang="en">
       <head>
@@ -20,16 +19,9 @@ export default function RootLayout({ children }) {
       </head>
       <body>
         <Provider>
-          <ThemeWrapper>
-            {/* <Link
-              href={`/`}
-              className=" mr-auto mt-5 px-4 py-2 border-solid border-red-700 border-2 rounded-md bg-red-700 text-white"
-            >
-              <button className="">HOME</button>
-            </Link> */}
+          {/* <ThemeWrapper> */}
           {children}
-          </ThemeWrapper>
-          
+          {/* </ThemeWrapper> */}
         </Provider>
       </body>
     </html>
