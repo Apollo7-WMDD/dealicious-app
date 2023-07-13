66bc3e88a11dfa240cfdcfff6b2475f34f6fc4b5
Mario Cesena <112787277+mariocc22@users.noreply.github.com>
2023-07-13 16:58:18 -0700

    chore: deploy: Deploy Settings

--- next.config.js
+++ next.config.js
@@ -16,20 +16,6 @@ const nextConfig = {
           },
         },
       ],
-      source: "/api/:path*",
-      headers: [
-        { key: "Access-Control-Allow-Credentials", value: "true" },
-        { key: "Access-Control-Allow-Origin", value: "*" },
-        {
-          key: "Access-Control-Allow-Methods",
-          value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-        },
-        {
-          key: "Access-Control-Allow-Headers",
-          value:
-            "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-        },
-      ],
     });
 
     return config;
@@ -37,21 +23,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-
-// module.exports = {
-//     webpack(config) {
-//       config.module.rules.push({
-//         test: /\.svg$/i,
-//         // issuer: /\.[jt]sx?$/,
-//         use: [
-//                 {
-//                   loader: "@svgr/webpack",
-//                   options: {
-//                     native: true,
-//                   },
-//                 },
-//               ],
-//       })
-//       return config
-//     },
-//   }
