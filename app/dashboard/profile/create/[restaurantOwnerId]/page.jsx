5b8c2c1b99eaa00ce836fc2b43979c609c27e366
Ziyun Yue <119447795+Lemon03@users.noreply.github.com>
2023-07-12 18:00:48 -0700

    Revert "feat: AP179-Create campaign page"

--- app/dashboard/profile/create/[restaurantOwnerId]/page.jsx
+++ app/dashboard/profile/create/[restaurantOwnerId]/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect, useTheme } from "react";
+import { useState, useEffect } from "react";
 import InputText from "../../../../components/Input/InputText";
 import Form from "../../../../components/Card/Form";
 import SubHeader from "../../../../components/Header/SubHeader";
@@ -163,24 +163,38 @@ const Restaurant = () => {
   const [referralSystemVisible, setreferralSystemVisible] = useState(true);
 
   return (
-      <div>
-        <Header>Create Profile</Header>
-            <Form>
-            <Box sx={{
-                display: 'flex', 
-                justifyContent: 'space-between',
-                alignContent: 'center',
-                flexDirection: 'row',
-                width: '100%',
-              }}>
-                <SubHeader sx={{fontSize:'20px'}}>Business Information</SubHeader>
-                <KeyboardArrowDown onClick={() => setBusinessInfoVisible(!businessInfoVisible)} />
-            </Box>
-              {businessInfoVisible && (
-              <>
-              <Box sx={{ display: { xs: 'block', md: 'flex', justifyContent:'space-between' }}}>
-                <Box sx={{ width: { xs: '100%', md: '63%' }, display: 'flex', flexDirection: { xs: 'column', md: 'row' }, flexWrap: 'wrap', justifyContent:'space-between'}}>
-                  <Box sx={{ width: { xs: '100%', md: '48%' }}}>
+    <div>
+      <Header>Create Profile</Header>
+        <Form>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignContent: "center",
+              flexDirection: "row",
+              width: "100%",
+            }}
+          >
+            <SubHeader sx={{ fontSize: "20px" }}>
+              Business Information
+            </SubHeader>
+            <KeyboardArrowDown
+              onClick={() => setBusinessInfoVisible(!businessInfoVisible)}
+            />
+          </Box>
+          {businessInfoVisible && (
+            <>
+              <Box sx={{ display: { xs: "block", md: "flex", justifyContent:'space-between' }}}>
+                <Box
+                  sx={{
+                    width: { xs: "100%", md: "63%" },
+                    display: "flex",
+                    flexDirection: { xs: "column", md: "row" },
+                    flexWrap: "wrap",
+                    justifyContent:'space-between',
+                  }}
+                >
+                  <Box sx={{ width: { xs: "100%", md: "48%" } }}>
                     <InputText
                       label="Business Name"
                       value={formData.name}
@@ -190,7 +204,7 @@ const Restaurant = () => {
                       placeholder="Business Name"
                     />
                   </Box>
-                  <Box sx={{ width: { xs: '100%', md: '48%' }}}>
+                  <Box sx={{ width: { xs: "100%", md: "48%" } }}>
                     <InputText
                       label="Business Category"
                       value={formData.category}
@@ -200,7 +214,7 @@ const Restaurant = () => {
                       placeholder="Ex:Japanese,Indian,Brunch"
                     />
                   </Box>
-                  <Box sx={{ width: { xs: '100%', md: '48%' }}}>
+                  <Box sx={{ width: { xs: "100%", md: "48%" } }}>
                     <InputText
                       label="Manager"
                       name="manager"
@@ -210,7 +224,7 @@ const Restaurant = () => {
                       placeholder="Manager"
                     />
                   </Box>
-                  <Box sx={{ width: { xs: '100%', md: '48%' }}}>
+                  <Box sx={{ width: { xs: "100%", md: "48%" } }}>
                     <InputText
                       label="Website"
                       name="website"
@@ -220,7 +234,7 @@ const Restaurant = () => {
                       placeholder="Website"
                     />
                   </Box>
-                  <Box sx={{ width: { xs: '100%', md: '48%' }}}>
+                  <Box sx={{ width: { xs: "100%", md: "48%" } }}>
                     <InputText
                       label="Business Email Address"
                       type="email"
@@ -231,7 +245,7 @@ const Restaurant = () => {
                       placeholder="Business Email Address"
                     />
                   </Box>
-                  <Box sx={{ width: { xs: '100%', md: '48%' }}}>
+                  <Box sx={{ width: { xs: "100%", md: "48%" } }}>
                     <InputText
                       label="Postal Code"
                       name="postalCode"
@@ -242,200 +256,184 @@ const Restaurant = () => {
                     />
                   </Box>
                 </Box>
-                <Box sx={{display: 'flex', flexDirection: { xs: 'column', md: 'row' }, flexWrap: 'wrap', justifyContent:'space-between'}}>
-                  <Box sx={{ width: { xs: '100%', md: '30.5%' }}}>
-                    <InputText
-                      label="Business Phone Number"
-                      type="tel"
-                      name="phone"
-                      id="phone"
-                      value={formData.phone}
-                      onChange={handleInputChange}
-                      placeholder="Business Phone Number"
-                    />
-                  </Box>
-                  <Box sx={{ width: { xs: '100%', md: '67%' }}}>
-                    <InputText
-                      label="Street Address"
-                      name="street"
-                      id="street"
-                      value={formData.address.street}
-                      onChange={handleAddressChange}
-                      placeholder="Street Address"
-                    />
-                  </Box>
-                  <Box sx={{ width: { xs: '100%', md: '30.5%' }}}>
-                    <InputText
-                      label="City"
-                      name="city"
-                      id="city"
-                      value={formData.address.city}
-                      onChange={handleAddressChange}
-                      placeholder="City"
-                    />
-                  </Box>
-                  <Box sx={{ width: { xs: '100%', md: '33.5%' }}}>
-                    <InputText
-                      label="Province/State"
-                      name="province"
-                      id="province"
-                      value={formData.address.province}
-                      onChange={handleAddressChange}
-                      placeholder="Province/State"
-                    />
-                  </Box>
-                  <Box sx={{ width: { xs: '100%', md: '30.5%'}}}>
-                    <InputDropdown
-                      label="Country"
-                      value={formData.address.country}
-                      onChange={handleAddressChange}
-                      name="country"
-                      id="country"
-                      placeholder="Country"
-                      options={[
-                        { value: 'Canada', label: 'Canada' },
-                        { value: 'USA', label: 'USA' },
-                      ]}
-                    />
-                  </Box>
+                <Box
+                  sx={{
+                    flex: { xs: "0 0 100%", md: "0 0 33%" },
+                  }}
+                >
+                  <PictureUploadCard
+                    sx={{ height: { md: "255px" } }}
+                    phrase="Upload Logo File"
+                    onFileSelected={uploadLogo}
+                  />
                 </Box>
-                </>
-              )}
-
-            </Form>
-            <Form>
-            <Box sx={{
-                display: 'flex', 
-                justifyContent: 'space-between',
-                alignContent: 'center',
-                flexDirection: 'row',
-                width: '100%',
-              }}>
-              <SubHeader>Business Hours</SubHeader>
-              <KeyboardArrowDown onClick={() => setBusinessHoursVisible(!businessHoursVisible)} />
-            </Box>
-              {businessHoursVisible && 
-                weekdays.map((day) => (
-                  <Box 
-                    key={day} 
-                    sx={{ 
-                      display: 'flex', 
-                      flexDirection: { xs: 'column', md: 'row' }, 
-                      gap: { xs: '0', md: '30px' },
-                      marginBottom: '20px', 
-                      alignItems: 'center' 
+              </Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: { xs: "column", md: "row" },
+                  flexWrap: "wrap",
+                  justifyContent:'space-between',
+                }}
+              >
+                <Box sx={{ width: { xs: "100%", md: "30.5%" } }}>
+                  <InputText
+                    label="Business Phone Number"
+                    type="tel"
+                    name="phone"
+                    id="phone"
+                    value={formData.phone}
+                    onChange={handleInputChange}
+                    placeholder="Business Phone Number"
+                  />
+                </Box>
+                <Box sx={{ width: { xs: "100%", md: "67%" } }}>
+                  <InputText
+                    label="Street Address"
+                    name="street"
+                    id="street"
+                    value={formData.address.street}
+                    onChange={handleAddressChange}
+                    placeholder="Street Address"
+                  />
+                </Box>
+                <Box sx={{ width: { xs: "100%", md: "30.5%" } }}>
+                  <InputText
+                    label="City"
+                    name="city"
+                    id="city"
+                    value={formData.address.city}
+                    onChange={handleAddressChange}
+                    placeholder="City"
+                  />
+                </Box>
+                <Box sx={{ width: { xs: "100%", md: "33.5%" } }}>
+                  <InputText
+                    label="Province/State"
+                    name="province"
+                    id="province"
+                    value={formData.address.province}
+                    onChange={handleAddressChange}
+                    placeholder="Province/State"
+                  />
+                </Box>
+                <Box
+                  sx={{ width: { xs: "100%", md: "30.5%"} }}
+                >
+                  <InputDropdown
+                    label="Country"
+                    value={formData.address.country}
+                    onChange={handleAddressChange}
+                    name="country"
+                    id="country"
+                    placeholder="Country"
+                    options={[
+                      { value: "Canada", label: "Canada" },
+                      { value: "USA", label: "USA" },
+                    ]}
+                  />
+                </Box>
+              </Box>
+            </>
+          )}
+        </Form>
+        <Form>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignContent: "center",
+              flexDirection: "row",
+              width: "100%",
+            }}
+          >
+            <SubHeader>Business Hours</SubHeader>
+            <KeyboardArrowDown
+              onClick={() => setBusinessHoursVisible(!businessHoursVisible)}
+            />
+          </Box>
+          {businessHoursVisible &&
+            weekdays.map((day) => (
+              <Box
+                key={day}
+                sx={{
+                  display: "flex",
+                  flexDirection: { xs: "column", md: "row" },
+                  gap: { xs: "0", md: "30px" },
+                  marginBottom: '20px', 
+                  alignItems: "center",
+                }}
+              >
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignContent: "center",
+                    width: { xs: "auto", md: "200px" },
+                    gap: "5px",
+                  }}
+                >
+                  <Typography
+                    sx={{
+                      color: "#181818",
+                      fontSize: "16px",
+                      fontFamily: "Mukta",
+                      fontStyle: "normal",
+                      fontWeight: 600,
+                      alignSelf: "center",
+                      length: "50px",
+                      marginRight: "10px",
                     }}
                   >
-                    <Box 
-                      sx={{ 
-                        display: 'flex', 
-                        justifyContent: 'space-between',
-                        alignContent: 'center',
-                        width: { xs: 'auto', md: '200px' },
-                        gap: '5px'
-                      }}
-                    >
-                      <Typography sx={{
-                        color: '#181818',
-                        fontSize: '16px',
-                        fontFamily: 'Mukta',
-                        fontStyle: 'normal',
-                        fontWeight: 600,
-                        alignSelf: 'center',
-                        length: '50px',
-                        marginRight: '10px',
-
-                      }}>{day.charAt(0).toUpperCase() + day.slice(1)}</Typography>
-                      <InputCheckbox 
-                        label="Closed"
-                        onChecked={DayClosedChange(day)}
-                        labelPlacement="start"
-                        checked={formData.businessHours[day].closed}
-                      />
-                    </Box>
-                    <TimeDropdown
-                      day={day}
-                      isDisabled={formData.businessHours[day].closed}
-                      setBusinessHours={(newHours) => {
-                        setFormData((prevState) => ({
-                          ...prevState,
-                          businessHours: {
-                            ...prevState.businessHours,
-                            [day]: newHours,
-                          },
-                        }));
-                      }}
-                    />
-                  </Box>
-                ))
-              }
-            </Form>
-
-          
-            <Form>
-              <Box sx={{
-                display: 'flex', 
-                justifyContent: 'space-between',
-                alignContent: 'center',
-                flexDirection: 'row',
-                width: '100%',
-              }}>
-                <SubHeader>Images & Menus</SubHeader>
-                <KeyboardArrowDown onClick={() => setImagesAndMenusVisible(!imagesAndMenusVisible)} />
-              </Box>
-              {imagesAndMenusVisible && (
-                <PictureUploadCard
-                  phrase="Upload Menus"
-                  onFileSelected={uploadMenu}
+                    {day.charAt(0).toUpperCase() + day.slice(1)}
+                  </Typography>
+                  <InputCheckbox
+                    label="Closed"
+                    onChecked={DayClosedChange(day)}
+                    labelPlacement="start"
+                    checked={formData.businessHours[day].closed}
+                  />
+                </Box>
+                <TimeDropdown
+                  day={day}
+                  isDisabled={formData.businessHours[day].closed}
+                  setBusinessHours={(newHours) => {
+                    setFormData((prevState) => ({
+                      ...prevState,
+                      businessHours: {
+                        ...prevState.businessHours,
+                        [day]: newHours,
+                      },
+                    }));
+                  }}
                 />
               </Box>
-              {referralSystemVisible && (
-                <>
-                 <Box sx={{
-                    display: 'flex', 
-                    flexDirection: { xs: 'column', md: 'row'},
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                    gap: '24px',
-                  }}>
-                    <Box sx={{    
-                      display: 'flex', 
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                      flexDirection: 'column',
-                      gap: '8px',
-                      width: { xs: '326px', md: '400px'},
-                      marginTop:'20px'}}>
-                      <Typography sx={{
-                        color: '#181818',
-                        fontSize: '19px',
-                        fontFamily: 'Mukta',
-                        fontStyle: 'normal',
-                        fontWeight: 600,
-                      }}>How many points the Super Customers can earn for each hundred dollar they spend?</Typography>
-                      <InputText
-                        name="points"
-                        id="points"
-                        value={formData.points}
-                        onChange={handleInputChange}
-                        placeholder="Points"
-                        style={{whiteSpace: 'nowrap'}}
-                      />
-                    </Box>
-                    <Box sx={{    
-                      display: 'flex', 
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                      flexDirection: 'row',
-                      gap: '5px',
-                      width: { xs: '326px', md: '400px'},}}>
-                      <ErrorOutlineIcon />
-                      <Typography>You set up this number only when you’re creating your profile. The Super Customers can redeem their points to get $1.00 discount per point.</Typography>
-                    </Box>
-                  </Box>
-                </>
-              )}
-            </Form>
+            ))}
+        </Form>
+
+        <Form>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignContent: "center",
+              flexDirection: "row",
+              width: "100%",
+            }}
+          >
+            <SubHeader>Images & Menus</SubHeader>
+            <KeyboardArrowDown
+              onClick={() => setImagesAndMenusVisible(!imagesAndMenusVisible)}
+            />
+          </Box>
+          {imagesAndMenusVisible && (
+            <PictureUploadCard
+              phrase="Upload Menus"
+              onFileSelected={uploadMenu}
+            />
+          )}
+        </Form>
+
         <Form>
           <Box
             sx={{
@@ -459,7 +457,7 @@ const Restaurant = () => {
                   flexDirection: { xs: "column", md: "row" },
                   justifyContent: "space-between",
                   alignItems: "center",
-                  gap: "8px",
+                  gap: "25px",
                 }}
               >
                 <Box
@@ -526,9 +524,9 @@ const Restaurant = () => {
           secondButtonText="Save Profile"
           type="submit"
         />
-      </form>
     </div>
   );
 };
 
 export default Restaurant;
+
