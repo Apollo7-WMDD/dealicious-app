75dc827059165edb28da010b6db4b96430f30850
Mario Cesena <112787277+mariocc22@users.noreply.github.com>
2023-07-13 17:42:51 -0700

    test_2

--- next.config.js
+++ next.config.js
@@ -2,10 +2,7 @@
 
 const nextConfig = {
   reactStrictMode: false,
-  webpack(config, { isServer }) {
-    if (!isServer) {
-      config.resolve.fallback.fs = false;
-    }
+  webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
       use: [
