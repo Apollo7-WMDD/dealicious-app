9f4460bf55a89652145f926ee49247fc5e5586d0
Edgar Velandia <evelandiarodri00@mylangara.ca>
2023-07-16 13:53:26 -0700

    feat:Adding SC Footer style

--- app/components/Footer/SCFooter.jsx
+++ app/components/Footer/SCFooter.jsx
@@ -5,34 +5,48 @@ import DealIcon from "@/app/components/svg/scdealicon.svg";
 function SCFooter() {
     const theme = useTheme();
   return (
-    <Box
-        sx={{
-            bgcolor: '#474747',//theme.palette.primary[60],
-            color: 'white',
-            height: "78px",
-            display: 'flex',
-            flex: 'flex-flow',
-            justifyContent: 'space-between'
-        }}
-    >
         <Box
+            sx={{
+                bgcolor: '#474747',//theme.palette.primary[60],
+                color: 'white',
+                height: "78px",
+                display: 'flex',
+                flex: 'flex-flow',
+                justifyContent: 'space-between',
+                alignItems:'center',
+                m:0,
+                p:'0 1rem',
+                bottom: 0,
+                position: 'fixed',
+                width:'100%',
+            }}
         >
-            <DealIcon />
-            <Typography>dealicious</Typography>
-        </Box>        
-        <Box
-        sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'flex-end',
-            gap: 5,
-        }}>            
-            <Typography>CONTACT US</Typography>
-            <Typography>FAQ</Typography>
-            <Typography>PRIVACY POLICY</Typography>
-            <Typography>TERMS OF USE</Typography>
+            <Box>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flex: 'flex-flow',
+                        // justifyContent: 'space-between',
+                    }}
+                >
+                    <DealIcon />
+                    <Typography>dealicious</Typography>
+                </Box>  
+                <Typography variant="p">&copy; 2023{" "} Team Apollo7. All Rights Reserved.</Typography>
+            </Box>      
+            <Box
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'flex-end',
+                    gap: 5,
+                }}>            
+                <Typography>CONTACT US</Typography>
+                <Typography>FAQ</Typography>
+                <Typography>PRIVACY POLICY</Typography>
+                <Typography>TERMS OF USE</Typography>
+            </Box>
         </Box>
-    </Box>
   );
 }
 
