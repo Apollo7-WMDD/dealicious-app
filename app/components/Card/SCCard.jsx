8d911bd81078ea796c7cdbd856d2b8d60fd84fbd
Edgar Velandia <evelandiarodri00@mylangara.ca>
2023-07-15 17:52:56 -0700

    AP-180: feat: Adding Header, Footer and SCCard

--- app/components/Card/SCCard.jsx
+++ app/components/Card/SCCard.jsx
@@ -1,39 +1,56 @@
 import React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import StarIcon from '@mui/icons-material/Star';
+import { Card, CardActions, CardContent, Box, Typography } from "@mui/material";
 
 const SCCard = ({ props }) => {
 
-//export default function SCCard({props}) {
-  return (
-    <div>
-      <Card sx={{ maxWidth: 345 }}>
+return (
+    <Box>
+      <Card 
+        sx={{ 
+          maxWidth: 324,
+          borderRadius: '16px'
+        }}>
         <img 
         src="https://media.istockphoto.com/id/1415525222/photo/fire-and-chinese-chefs.jpg?s=1024x1024&w=is&k=20&c=zdg6cOC9XSu1Xw9Adys82VBZxHWkHupFiAqxBbxyqxU="
-        alt="new"
+        alt="new" height="189px"
         />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {props.name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {props._id}
-          </Typography>
-        </CardContent>
-        <img 
-        src={props.logo}
-        alt="new" width="50px" height="50px"
-        />
-        <CardActions>
-          <Button size="small">Share</Button>
-          <Button size="small">Learn More</Button>
-        </CardActions>
+        <Box
+          sx={{
+            display:'flex',
+          }}
+          >
+          <img 
+          src={props.logo}
+          alt="new" width="30px" height="30px"
+          style={{ borderRadius: '50%' }}
+          />
+          <CardContent
+            sx={{
+              p:0,
+              m:0,
+              textAlign:'center',
+            }}
+          >
+            <Typography gutterBottom variant="h5" component="div">
+              {props.name}
+            </Typography>
+            <Typography variant="p" color="text.secondary">
+              4.5<StarIcon />{props.campaigns.length} Active campaigns
+            </Typography>
+          </CardContent>
+        </Box>
+        <Box
+          sx={{
+            textAlign:'end',
+            p:1,
+            bgcolor: '#ffd8c8'
+          }}
+        >
+          <Typography>Points earned {props.points[0].points}</Typography>
+        </Box>
       </Card>
-    </div>
+    </Box>
   );
 };
 
--- app/superCustomer/restaurants/[superCustomerId]/page.jsx
+++ app/superCustomer/restaurants/[superCustomerId]/page.jsx
@@ -2,6 +2,8 @@
 import Header from "../../../components/Header/Header";
 import SCCard from "../../../components/Card/SCCard";
 import SCHeader from "../../../components/Header/SCHeader"
+import SCFooter from "../../../components/Footer/SCFooter"
+import { Box } from "@mui/material";
 
 const fetchRestaurants = async (superCustomerId) => {
   const isProduction = process.env.NODE_ENV === "production";
@@ -25,15 +27,33 @@ const fetchRestaurants = async (superCustomerId) => {
 const Page = async ({ params }) => {
   const { superCustomerId } = params;
   const data = await fetchRestaurants(superCustomerId);
-  console.log(data[0].name)
+  console.log(data);
+  //const activeCampaigns = data[0].filter(campaign => campaign.status === 'active');
+  //console.log(activeCampaigns);
+
   const cards = data.map((item, index) => <SCCard key={index} props={item} />);
 
   return (
-    <div>
+    <Box>
       <SCHeader />
-      <Header props={"My Restaurants"} />
-      {cards}
-    </div>
+      <Box
+        sx={{
+          p:'3%'
+        }}
+      >
+        <Header props={"My Restaurants"} />
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            gap:"3%",
+          }}
+        >
+          {cards}
+        </Box>
+      </Box>
+      <SCFooter />
+    </Box>
   );
 };
 
