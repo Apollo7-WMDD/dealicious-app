ed45c30bd4e4d499ebe930df1377a5a5fa97d205
Mario Cesena <112787277+mariocc22@users.noreply.github.com>
2023-07-17 16:09:30 -0700

    chore: AP-##: Pending

--- app/newCustomer/[restaurantId]/[superCustomerId]/page.jsx
+++ app/newCustomer/[restaurantId]/[superCustomerId]/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+
 import Link from "next/link";
 import Image from "next/image";
 import SCRestaurantCard from "../../../components/SuperCustomer/SCRestaurantCard";
@@ -7,51 +8,55 @@ import SCFooter from "../../../components/Footer/SCFooter";
 import CampaignCard from "@/app/components/SuperCustomer/CampaignCard";
 import NewCustomer from "@/app/components/NewCustomer/NewCustomer";
 import { Box } from "@mui/material";
+import { useEffect } from "react";
 
-const fetchRestaurant = async (superCustomerId, restaurantId) => {
-  const isProduction = process.env.NODE_ENV === "production";
-  const serverUrl = isProduction
-    ? process.env.NEXT_PUBLIC_SERVER_URL
-    : "http://localhost:3000";
-
-  const res = await fetch(
-    `${serverUrl}/api/superCustomer/newCustomer/${superCustomerId}/${restaurantId}`,
-    {
-      cache: "no-store",
-    }
-  );
-
-  if (!res.ok) throw new Error("Something went wrong...");
-
-  const data = await res.json();
-  return data;
-};
-
-const Page = async ({ params }) => {
+const Page = ({ params }) => {
   const { superCustomerId, restaurantId } = params;
-  const restaurantData = await fetchRestaurant(superCustomerId, restaurantId);
-  console.log(restaurantData)
+  const [restaurantData, setRestaurantData] = useState([]);
 
-  const cards = restaurantData.campaigns.map((item, index) => <CampaignCard key={index} props={item} />);
+  useEffect(() => {
+    const fetchRestaurant = async (superCustomerId, restaurantId) => {
+      const res = await fetch(
+        `/api/superCustomer/newCustomer/${superCustomerId}/${restaurantId}`,
+        {
+          cache: "no-store",
+        }
+      );
+
+      if (!res.ok) throw new Error("Something went wrong...");
+
+      const data = await res.json();
+      setRestaurantData(data);
+    };
+
+    fetchRestaurant(superCustomerId, restaurantId);
+    console.log(restaurantData);
+  }, [superCustomerId, restaurantId]);
 
   return (
     <>
       <SCHeader />
       <Box
         sx={{
-          display:'flex',
-          flexDirection:'row',
-          gap: '3%',
-          m:'1rem',
-          p:0,
+          display: "flex",
+          flexDirection: "row",
+          gap: "3%",
+          m: "1rem",
+          p: 0,
         }}
       >
         <NewCustomer props={restaurantData.user}></NewCustomer>
         <SCRestaurantCard props={restaurantData.restaurant} />
       </Box>
       <Box>
-        {cards}
-      </Box> 
+        {!restaurantData ? (
+          <h1>Loading...</h1>
+        ) : (
+          restaurantData.campaigns.map((item, index) => (
+            <CampaignCard key={index} props={item} />
+          ))
+        )}
+      </Box>
       <SCFooter />
     </>
   );
