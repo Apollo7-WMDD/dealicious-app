4b6de069c422fcb3c7895012b1238e2396638d69
Tony-Thawatchai <au.thawatchai@gmail.com>
2023-07-17 16:08:50 -0700

    feat:AP-211:owner/burncode GET *unsuccessfull

--- app/dashboard/burnCode/[restaurantOwnerId]/page.jsx
+++ app/dashboard/burnCode/[restaurantOwnerId]/page.jsx
@@ -2,23 +2,27 @@
 import Link from "next/link";
 import React from "react";
 import Header from "../../../components/Header/Header";
+import BurnCodeWrap from "../../../components/burnCode/BurnCodeWrap";
 
 
-const Page = ({ params }) => {
-  const { restaurantOwnerId } = params;
+const Page = () => {
+ 
+
+
+  
   return (
     <>
       
 
       <Header props={"Burn a Code"} />
-      
+      <BurnCodeWrap></BurnCodeWrap>
       
        
-      <Link href={`/dashboard/campaigns/active/${restaurantOwnerId}`}>
+      {/* <Link href={`/dashboard/campaigns/active/${restaurantOwnerId}`}>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded block m-4">
           Dashboard
         </button>
-      </Link>
+      </Link> */}
     </>
   );
 };
