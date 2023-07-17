d3f9a1bb7295fc73dbecd862ef17761f7099ca82
Mario Cesena <112787277+mariocc22@users.noreply.github.com>
2023-07-16 18:20:22 -0700

    feat: AP-202: Get requests for Profile view page

--- app/components/OwnerProfile/Images.jsx
+++ app/components/OwnerProfile/Images.jsx
@@ -1,21 +1,43 @@
-import ChartCard from '../Card/ChartCard';
-import SubHeader from '../Header/SubHeader';
-import { Box } from '@mui/material';
-import Typography from '@mui/material/Typography';
+import ChartCard from "../Card/ChartCard";
+import SubHeader from "../Header/SubHeader";
+import { Box } from "@mui/material";
+import Typography from "@mui/material/Typography";
 
-function BusinessHours() {
+import { useState, useEffect } from "react";
+import LoaderSkeleton from "./Loader/LoaderSkeleton";
+import { fetchImagesMenus } from "@/lib/fetching/profile/data";
+
+function ImagesMenus({ restaurantOwnerId }) {
+  const [restaurantData, setRestaurantData] = useState(null);
+
+  useEffect(() => {
+    const getRestaurantData = async () => {
+      const data = await fetchImagesMenus(restaurantOwnerId);
+      const { restaurantInfo } = data;
+      setRestaurantData(restaurantInfo);
+    };
+    getRestaurantData();
+  }, [restaurantOwnerId]);
+
   return (
-    <ChartCard content="Business Hours" gridColumn="1 / 3">
-      <Box sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'flex-start',
-          }}>
+    <ChartCard content="Images & Menus" gridColumn="1 / 3">
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-start",
+        }}
+      >
         <SubHeader>Images & Menus</SubHeader>
-        <Typography>test</Typography>
+        <Typography>
+          {!restaurantData ? <LoaderSkeleton /> : restaurantData?.menu}
+        </Typography>
+        <Typography>
+          {!restaurantData ? <LoaderSkeleton /> : restaurantData?.logo}
+        </Typography>
       </Box>
     </ChartCard>
   );
 }
 
-export default BusinessHours;
+export default ImagesMenus;
--- app/dashboard/profile/[restaurantOwnerId]/page.jsx
+++ app/dashboard/profile/[restaurantOwnerId]/page.jsx
@@ -3,122 +3,83 @@ import Header from "@/app/components/Header/Header";
 import { usePathname } from "next/navigation";
 // user context
 import { useStore } from "@/lib/context/user_context/store";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import SingleButtonNoIcon from "@/app/components/Button/SingleButtonNoIcon";
 import BusinessInfo from "@/app/components/OwnerProfile/BusinessInfo";
 import BusinessHours from "@/app/components/OwnerProfile/BusinessHours";
 import Images from "@/app/components/OwnerProfile/Images";
 import Referral from "@/app/components/OwnerProfile/Referral";
 import NavbarMobile from "@/app/components/Profile/NavbarMobile";
-import {Box, Grid} from '@mui/material';
-import { useTheme } from '@mui/material/styles';
-import useMediaQuery from '@mui/material/useMediaQuery';
+import { Box, Grid } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import { useRouter } from "next/navigation";
-// const fetchOwnerInfo = async (restaurantOwnerId) => {
-//   const isProduction = process.env.NODE_ENV === "production";
-//   const serverUrl = isProduction
-//     ? process.env.NEXT_PUBLIC_SERVER_URL
-//     : "http://localhost:3000";
 
-//   const res = await fetch(
-//     `${serverUrl}/api/dashboard/profile/${restaurantOwnerId}`
-//     // {
-//     //   cache: "no-store",
-//     // }
-//   );
-
-//   if (!res.ok) throw new Error("Something went wrong...");
-
-//   const data = await res.json();
-//   return data;
-// };
-
-const Page = async () => {
+const Page = () => {
   const router = useRouter();
-  // const { restaurantOwnerId } = params;
-  // const ownerData = await fetchOwnerInfo(restaurantOwnerId);
-  // console.log("owner data: ", ownerData);
-  
-  // const { restaurantOwnerId,setRestaurantOwner} = useStore();
-  // const pathname = usePathname();
-  // const URLrestaurantOwnerId = pathname.split("/")[3];
-  // useEffect(() => {
-  //   const setRestaurantOwnerFromParam =  () => {
-  //     setRestaurantOwner(URLrestaurantOwnerId);
-  //   };
-  //   setRestaurantOwnerFromParam();
-  // }, [URLrestaurantOwnerId]);
-  // console.log("restaurantOwnerId: ", restaurantOwnerId);
-  
-  
+  const { restaurantOwnerId } = useStore();
+
   const editClick = () => {
     router.push(`/dashboard/profile/edit/${restaurantOwnerId}`);
   };
 
   const [tab, setTab] = useState(0);
-
-  // useEffect(() => {
-  //   setRestaurantOwner(URLrestaurantOwnerId);
-  //   (async () => {
-  //     const ownerData = await fetchOwnerInfo(restaurantOwnerId);
-  //     console.log("owner data: ", ownerData);
-  //   })();
-  // }, [URLrestaurantOwnerId, restaurantOwnerId]);
-
   const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
   return (
     <>
       <Header props={"Profile"}></Header>
       {isMobile && (
         <>
-        <NavbarMobile value={tab} setValue={setTab} />
-        {tab === 0 && <BusinessInfo />}
-        {tab === 1 && (
-          <>
-            <BusinessHours />
-            <Box sx={{marginTop:'17px'}}>
-              <Images />
-            </Box>
-          </>
-        )}
-        {tab === 2 && <Referral />}
+          <NavbarMobile value={tab} setValue={setTab} />
+          {tab === 0 && <BusinessInfo />}
+          {tab === 1 && (
+            <>
+              <BusinessHours />
+              <Box sx={{ marginTop: "17px" }}>
+                <Images />
+              </Box>
+            </>
+          )}
+          {tab === 2 && <Referral />}
         </>
       )}
 
       {!isMobile && (
-        <Box sx={{ margin:{ md: '35px 70px' }}}>
-          <Grid container spacing={2} >
+        <Box sx={{ margin: { md: "35px 70px" } }}>
+          <Grid container spacing={2}>
             <Grid item xs={12} md={4}>
-              <BusinessInfo />
+              <BusinessInfo restaurantOwnerId={restaurantOwnerId} />
             </Grid>
             <Grid item xs={12} md={8}>
               <Grid container spacing={2}>
                 <Grid item xs={12} md={6}>
-                  <BusinessHours />
+                  <BusinessHours restaurantOwnerId={restaurantOwnerId} />
                 </Grid>
                 <Grid item xs={12} md={6}>
-                  <Referral />
+                  <Referral restaurantOwnerId={restaurantOwnerId} />
                 </Grid>
                 <Grid item xs={12}>
-                  <Images />
+                  <Images restaurantOwnerId={restaurantOwnerId} />
                 </Grid>
               </Grid>
             </Grid>
           </Grid>
         </Box>
       )}
-      <Box sx={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: { xs:'center', md: 'flex-end'},
-          margin: '20px',
-        }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: { xs: "center", md: "flex-end" },
+          margin: "20px",
+        }}
+      >
         <SingleButtonNoIcon
-          text="Edit" 
-          onClick={editClick} 
-          width={{ xs: '100%', md: '174px' }}
+          text="Edit"
+          onClick={editClick}
+          width={{ xs: "100%", md: "174px" }}
         />
       </Box>
     </>
