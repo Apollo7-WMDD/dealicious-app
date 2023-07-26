e90c9bde53f14ae67b75a6dc7b5cd86c8330ed61
Edgar Velandia <alvelx@hotmail.com>
2023-07-26 10:51:21 -0700

    fix:NC Style and button AP-266

--- app/components/Button/SCActive.jsx
+++ app/components/Button/SCActive.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button, Box, useTheme, Typography } from "@mui/material";
 import DealIcon from "@/app/components/svg/dealIcon.svg";
+import ButtonCampaignNew from "../Button/ButtonCampaignNew";
 
 const SCActive = ({ text, width, onClick }) => {
   const theme = useTheme();
@@ -8,21 +9,63 @@ const SCActive = ({ text, width, onClick }) => {
     <Button
       variant="contained"
       size="medium"
-      startIcon={<DealIcon />}
+      startIcon={<ButtonCampaignNew />}
       onClick={onClick}
       sx={{
         width: width,
-        height: "44px",
+        height: "70%",
+        boxShadow: "none",
         justifySelf: "end",
         alignSelf: "center",
         borderRadius: "12px",
+        backgroundColor:'white',
         border: '3px solid #ff5938',
-        backgroundColor: 'white',
         color: '#ff5938',
+        // backgroundColor: theme.palette.primary[80],
+        ":hover": {
+          backgroundColor: "#ff5938",
+          color: 'white',
+          boxShadow: "none",
+        },
+        [theme.breakpoints.down("lg")]: {
+          width: "265px",
+          fontSize: "16px",
+        },
+        [theme.breakpoints.down("md")]: {
+          width: "180px",
+          fontSize: "14px",
+          lineHeight: "16px",
+          margin: "1rem 0",
+          // alignSelf: "start",
+          justifySelf: "start",
+        },
       }}
     >
-    <Typography variant="p">{text}</Typography>
+      {text}
     </Button>
+    // <Button
+    //   variant="contained"
+    //   size="medium"
+    //   startIcon={<DealIcon />}
+    //   onClick={onClick}
+    //   sx={{
+    //     width: width,
+    //     height: "44px",
+    //     justifySelf: "end",
+    //     alignSelf: "center",
+    //     borderRadius: "12px",
+    //     border: '3px solid #ff5938',
+    //     backgroundColor: 'white',
+    //     color: '#ff5938',
+    //     fontWeight: 'bold',
+    //     '&:hover': {
+    //       color: 'white',
+    //       backgroundColor: '#ff5938', // Change this to the desired hover color
+    //     },
+    //   }}
+    // >
+    // <Typography variant="p">{text}</Typography>
+    // </Button>
   );
 };
 
--- app/newCustomer/[restaurantId]/[superCustomerId]/page.jsx
+++ app/newCustomer/[restaurantId]/[superCustomerId]/page.jsx
@@ -1,7 +1,4 @@
 "use client";
-
-import Link from "next/link";
-import Image from "next/image";
 import SCRestaurantCard from "@/app/components/SuperCustomer/SCRestaurantCard";
 import NCHeader from "@/app/components/Header/NCHeader";
 import SCFooter from "@/app/components/Footer/SCFooter";
@@ -58,27 +55,12 @@ const Page = ({ params }) => {
           "@media screen and (min-width:800px)": {
             display: "grid",
             gridTemplateColumns:'1fr 2fr',
-            // gridColumn: '1fr 2fr,'
-            // gridAutoColumns:'1fr 2fr'
-            // flexDirection: "row",
           },
         }}
       >
         <NewCustomer 
-          // sx={{
-          //   "@media screen and (min-width:800px)": {
-          //     flexGrow: 1,
-          //     flexShrink: 0,
-          //   },
-          // }}        
           props={restaurantData?.user}></NewCustomer>
         <SCRestaurantCard 
-          // sx={{
-          //   "@media screen and (min-width:800px)": {
-          //     flexGrow: 2,
-          //     flexShrink: 1,
-          //   },
-          // }} 
         {...restaurantData?.restaurant} />        
       </Box>
       <Box
@@ -132,14 +114,6 @@ const Page = ({ params }) => {
           ) : (
             restaurantData.campaigns.map((item, index) => (
               <CampaignCard
-                sx={
-                  {
-                    // flexGrow: '1',
-                    // flexShrink: '1',
-                    // flexBasis: '100%',
-                    // flex: '1 0 40%',
-                  }
-                }
                 key={index}
                 props={item}
               />
@@ -147,71 +121,9 @@ const Page = ({ params }) => {
           )}
         </Box>
       </Box>
-      {/* <Box>
-        {!restaurantData ? (
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              width: "100%",
-              height: "100%",
-            }}
-          >
-            <Loader />
-          </div>
-        ) : (
-          restaurantData?.campaigns?.map((item, index) => (
-            <CampaignCard key={index} props={item} />
-          ))
-        )}
-      </Box> */}
       <SCFooter />
     </>
   );
 };
 
 export default Page;
-// import Link from "next/link";
-
-// const fetchRestaurant = async (restaurantId, superCustomerId) => {
-//   const isProduction = process.env.NODE_ENV === "production";
-//   const serverUrl = isProduction
-//     ? process.env.NEXT_PUBLIC_SERVER_URL
-//     : "http://localhost:3000";
-
-//   const res = await fetch(
-//     `${serverUrl}/api/newCustomer/${restaurantId}/${superCustomerId}`
-//     // {
-//     //   cache: "no-store",
-//     // }
-//   );
-
-//   if (!res.ok) throw new Error("Something went wrong...");
-
-//   const data = await res.json();
-//   return data;
-// };
-
-// const Page = async ({ params }) => {
-//   const { restaurantId, superCustomerId } = params;
-//   const restaurantData = await fetchRestaurant(restaurantId, superCustomerId);
-//   console.log(restaurantData);
-
-//   return (
-//     <>
-//       <h1>
-//         New Customer page that shows all the Campaigns of a restaurant that a
-//         Super Customer shared his special link with you from that specific
-//         restaurant
-//       </h1>
-//       <Link href={`/home/${restaurantId}`}>
-//         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded block m-4">
-//           Home - Restaurant
-//         </button>
-//       </Link>
-//     </>
-//   );
-// };
-
-// export default Page;
