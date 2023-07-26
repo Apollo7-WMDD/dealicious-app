8f366e380b98ee6c15f95300b6513d23dd5db5c9
Mario Cesena <112787277+mariocc22@users.noreply.github.com>
2023-07-26 09:57:23 -0700

    feat: AP-264: Animation SVG for button

--- app/components/Button/ButtonCampaignNew.jsx
+++ app/components/Button/ButtonCampaignNew.jsx
@@ -1,18 +1,7 @@
-import DealIcon from "@/app/components/svg/dealIcon.svg";
-import "./ButtonCampaign.css";
-
-const style = {
-  display: "flex",
-  animation:
-    "rotateAnimation 1s infinite alternate cubic-bezier(0.25, 0.1, 0.25, 1)",
-};
+import CampaignNewIcon from "./CampaignNewIcon";
 
 const ButtonCampaignNew = () => {
-  return (
-    <div style={style}>
-      <DealIcon />
-    </div>
-  );
+  return <CampaignNewIcon />;
 };
 
 export default ButtonCampaignNew;
