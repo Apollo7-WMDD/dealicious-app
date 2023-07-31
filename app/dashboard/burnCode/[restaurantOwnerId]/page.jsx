10f7b645ba4415f3832f5686e6fff91e087eefa6
Mario Cesena <112787277+mariocc22@users.noreply.github.com>
2023-07-31 16:49:59 -0700

    chore: AP-277: Fixing the re-rendering codes

--- app/dashboard/burnCode/[restaurantOwnerId]/page.jsx
+++ app/dashboard/burnCode/[restaurantOwnerId]/page.jsx
@@ -1,19 +1,24 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import { useState } from "react";
 import Header from "../../../components/Header/Header";
 import BurnCodeWrap from "../../../components/burnCode/BurnCodeWrap";
 import HeaderGrid from "@/app/components/HeaderGrid";
 import CreateNewCampaign from "@/app/components/Dashboard/CreateNewCampaign";
 
 const Page = () => {
+  const [render, setRender] = useState(false);
   return (
     <>
       <HeaderGrid>
         <Header props={"Burn a Code"} />
         <CreateNewCampaign />
       </HeaderGrid>
-      <BurnCodeWrap />
+      <BurnCodeWrap
+        key={render ? "render" : "not-render"}
+        render={render}
+        setRender={setRender}
+      />
     </>
   );
 };
