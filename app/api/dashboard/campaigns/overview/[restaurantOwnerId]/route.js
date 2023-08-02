eeac2da8637c57fd358baa8b76f269e62491cd62
Mario Cesena <112787277+mariocc22@users.noreply.github.com>
2023-08-02 13:36:55 -0700

    chore: AP-##: Remove comments

--- app/api/dashboard/campaigns/overview/[restaurantOwnerId]/route.js
+++ app/api/dashboard/campaigns/overview/[restaurantOwnerId]/route.js
@@ -11,20 +11,19 @@ export const GET = async (request) => {
   try {
     await connect();
 
-    // Find restaurantId from the restaurant collection
+    // 1. Find restaurantId from the restaurant collection
     const restaurant = await Restaurant.findOne(
       { userId: restaurantOwner },
       { _id: 1 }
     ).lean();
 
     if (!restaurant) {
-      // Return empty response if no restaurant info is found
+      // 2. Return empty response if no restaurant info is found
       return new NextResponse(JSON.stringify({ data: null }), { status: 200 });
     }
 
     const restaurantId = restaurant._id;
 
-    // Find the campaigns for the restaurant and get the total revenue and customers, the revenue is in billamount and customers is in the number of spendings for the restaurantId, return the data in an object with the name of the campaign, the total revenue, the sum of all the spendings for the restaurant and the numbers of spendings for the restaurant, the billamount is in the spending collection
     const campaigns = await Campaign.aggregate([
       { $match: { restaurantId: restaurantId } },
       {
@@ -56,17 +55,18 @@ export const GET = async (request) => {
       },
     ]);
 
-    // also return the sum of all the totalBillAmount and the sum of all the count
+    // 3.  return the sum of all the totalBillAmount and the sum of all the count
     const totalRevenue = campaigns.reduce(
       (acc, campaign) => acc + campaign.totalBillAmount,
       0
     );
+
     const totalCustomers = campaigns.reduce(
       (acc, campaign) => acc + campaign.count,
       0
     );
 
-    // return the data in an object with the campaigns, the total revenue and the total customers
+    // 4. return the data in an object with the campaigns, the total revenue and the total customers
     return new NextResponse(
       JSON.stringify({
         campaigns,
