532535e1dee2c10944e1ead3d5c2d8654605a21c
Tony-Thawatchai <au.thawatchai@gmail.com>
2023-08-06 18:21:01 -0700

    bug:AP-361:ongoing/upcoming title alignment

---
+++ app/components/Card/ChartCardOngoingUpcoming.jsx
@@ -0,0 +1,33 @@
+import { Box } from "@mui/material";
+import { useTheme } from "@mui/material";
+
+function ChartCard({ children, gridColumn }) {
+  const theme = useTheme();
+  const shadowColor = `${theme.palette.neutral[20]}1f`;
+
+  return (
+    <Box
+      sx={{
+        gridColumn: gridColumn,
+        boxShadow: `0px 4px 20px 0px ${shadowColor}`,
+        borderRadius: "8px",
+        display: "flex",
+        padding: "1rem 1.5rem",
+        flexDirection: "column",
+        justifyContent: "start",
+        // justifyContent: "start",
+        alignItems: "center",
+        flexShrink: 0,
+        gap: ".5rem",
+        minHeight: "310px",
+        [theme.breakpoints.down("md")]: {
+          gridColumn: "1/-1",
+        },
+      }}
+    >
+      {children}
+    </Box>
+  );
+}
+
+export default ChartCard;
